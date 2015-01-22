37cd590656b61f7bf8a8447826c4e0def89c4e5f
Rafael Nowrotek <mail@benignware.com>
2015-01-22 19:42:00 +0100

    Separated logic and plugin entry

---
+++ wp_bootstrap_hooks_plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ Plugin Name: Bootstrap Hooks
+ Plugin URI: http://github.com/benignware/wp-asset-manifest
+ Description: A collection of action and filters to fit bootstrap into wordpress
+ Version: 0.0.1
+ Author: Rafael Nowrotek, Benignware
+ Author URI: http://benignware.com
+ License: MIT
+*/
+
+
+require_once "wp_bootstrap_hooks.php";
