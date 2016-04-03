c493672f7e8268c39b6fdb7b1a1fda46ce67f916
Rafael Nowrotek <mail@benignware.com>
2016-04-03 19:15:02 +0200

    Updated plugin information

--- wp_bootstrap_hooks_plugin.php
+++ wp_bootstrap_hooks_plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
  Plugin Name: Bootstrap Hooks
- Plugin URI: http://github.com/benignware/wp-asset-manifest
- Description: A collection of action and filters to fit bootstrap into wordpress
+ Plugin URI: http://github.com/benignware/wp-bootstrap-hooks
+ Description: A collection of action and filters for bootstrap based themes
  Version: 0.0.1
  Author: Rafael Nowrotek, Benignware
  Author URI: http://benignware.com
