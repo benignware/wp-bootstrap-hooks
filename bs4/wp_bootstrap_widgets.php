b23f26f59642b8467d48a822c855bef3fc4ce558
Rafael Nowrotek <mail@benignware.com>
2016-04-08 20:16:07 +0200

    Fixed search widget appearance

--- bs4/wp_bootstrap_widgets.php
+++ bs4/wp_bootstrap_widgets.php
@@ -136,19 +136,18 @@ function wp_bootstrap_widgets_init() {
 }
 
 
-
-
 function wp_bootstrap_get_search_form( $form ) {
   $form = '<form role="search" method="get" id="searchform" class="searchform" action="' . home_url( '/' ) . '" >
   <label class="screen-reader-text" for="s">' . __( 'Search for:' ) . '</label>
-  <div class="input-group">
-  <input class="form-control" type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="'. esc_attr__( 'Search' ) .'..."/>
-  <span class="input-group-btn">
-    <button class="btn btn-default" type="submit" id="searchsubmit" title="' . esc_attr_x( 'Search', 'submit button' ) . '"><i class="glyphicon glyphicon-search"> </i></button>
-  </span>
+  <div class="form-group">
+    <div class="input-group">
+    <input class="form-control" type="text" value="' . get_search_query() . '" name="s" id="s" placeholder="'. esc_attr__( 'Search' ) .'..."/>
+    <span class="input-group-btn">
+      <button class="btn btn-default" type="submit" id="searchsubmit" title="' . esc_attr_x( 'Search', 'submit button' ) . '"><i>🔎</i></button>
+    </span>
+    </div>
   </div>
   </form>';
-
   return $form;
 }
 
