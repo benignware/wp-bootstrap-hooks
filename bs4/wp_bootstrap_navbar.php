512526b96c7a4cc151cf0c5afdf24b5a240032d5
Rafael Nowrotek <mail@benignware.com>
2016-04-27 20:30:07 +0200

    Use navbar for primary theme location

--- bs4/wp_bootstrap_navbar.php
+++ bs4/wp_bootstrap_navbar.php
@@ -3,23 +3,23 @@
 require('wp_bootstrap_navwalker.php');
 
 function wp_bootstrap_nav_menu_args($args) {
+  // Navbar walker does only apply to primary menu
+  
+  $menu_class = isset($args['menu_class']) ? $args['menu_class'] . ' ' : '';
   
   $args = array_merge($args, array(
-    //'container'         => 'div',
-    //'container_class'   => 'collapse navbar-collapse',
-    //'container_id'      => 'navbar-collapse',
-    'menu_class'        => 'nav navbar-nav'
+    'menu_class'        => $menu_class . 'nav'
   ));
   
+  if (isset($args['theme_location']) && trim($args['theme_location']) === 'primary') {
+    $args['menu_class'] = $args['menu_class'] . " navbar-nav"; 
+  }
+  
   if (empty($args['walker'])) {
-    
     $args['fallback_cb'] = 'wp_bootstrap_navwalker::fallback';
     $args['walker'] = new wp_bootstrap_navwalker();
   }
   
   return $args;
 }
-
-
-
 ?>
