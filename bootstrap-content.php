ff91cb220c49ff8865a627a04cd86597adf7c615
Rafael Nowrotek <mail@benignware.com>
2016-10-14 00:06:36 +0200

    Add handling of img captions, blockquotes and tables

--- bootstrap-content.php
+++ bootstrap-content.php
@@ -4,39 +4,214 @@
  */
 function wp_bootstrap_get_content_options() {
   return apply_filters( 'bootstrap_content_options', array(
-    'image_align_left_class' => 'pull-left',
-    'image_align_right_class' => 'pull-right',
-    'image_align_center_class' => 'center-block'
+    'image_class' => 'img-fluid img-responsive',
+    'align_left_class' => 'pull-left',
+    'align_right_class' => 'pull-right',
+    'align_center_class' => 'center-block m-x-auto',
+    'img_caption_tag' => 'figure',
+    'img_caption_class' => 'figure',
+    'img_caption_text_tag' => 'figcaption',
+    'img_caption_text_class' => 'figure-caption',
+    'img_caption_img_class' => 'figure-img',
+    'table_class' => 'table',
+    'blockquote_class' => 'blockquote',
+    'blockquote_footer_tag' => 'footer',
+    'blockquote_footer_class' => 'blockquote-footer'
   ));
 }
 
 /**
- * Add bootstrap classes to content images
+ * The content
  */
 if(!function_exists('wp_bootstrap_the_content')) {
+  
   function wp_bootstrap_the_content($content) {
     $options = wp_bootstrap_get_content_options();
-    $image_align_left_class = $options['image_align_left_class'];
-    $image_align_right_class = $options['image_align_right_class'];
-    $image_align_center_class = $options['image_align_center_class'];
-    $html = new DOMDocument();
-    @$html->loadHTML('<?xml encoding="utf-8" ?>' . $content );
-    $image_nodes = $html->getElementsByTagName( 'img' );
-    foreach ($image_nodes as $image_node) {
-      $class = $image_node->getAttribute('class');
-      if (strpos($class, 'alignleft') !== false) {
-        $class.= " $image_align_left_class";
+
+    // Extract options
+    extract($options);
+    
+    // Parse DOM
+    $doc = new DOMDocument();
+    @$doc->loadHTML('<?xml encoding="utf-8" ?>' . $content );
+    //$doc_xpath = new DOMXpath($doc);
+    
+    // Images
+    $image_elements = $doc->getElementsByTagName( 'img' );
+    foreach ($image_elements as $image_element) {
+      // Adjust class
+      $image_element_class = $image_element->getAttribute('class');
+      $image_element_class = trim($image_element_class . " " . $image_class);
+      if (preg_match('/\balignleft\b/i', $image_element_class)) {
+        $image_element_class.= " $align_left_class";
+      } else if (preg_match('/\balignright\b/i', $image_element_class)) {
+        $image_element_class.= " $align_right_class";
+      } else if (preg_match('/\baligncenter\b/i', $image_element_class)) {
+        $image_element_class.= " $align_center_class";
       }
-      if (strpos($class, 'alignright') !== false) {
-        $class.= " $image_align_right_class";
-      }
-      if (strpos($class, 'aligncenter') !== false) {
-        $class.= " $image_align_center_class";
-      }
-      $image_node->setAttribute('class', $class);
+      $image_element->setAttribute('class', $image_element_class);
     }
-    return preg_replace('~(?:<\?[^>]*>|<(?:!DOCTYPE|/?(?:html|head|body))[^>]*>)\s*~i', '', $html->saveHTML());
+    
+    // Blockquotes
+    $blockquote_elements = $doc->getElementsByTagName( 'blockquote' );
+    foreach ($blockquote_elements as $blockquote_element) {
+      // Add blockquote class
+      $blockquote_element_class = $blockquote_element->getAttribute('class');
+      $blockquote_element_class = trim($blockquote_element_class . " " . $blockquote_class);
+      $blockquote_element->setAttribute('class', $blockquote_element_class);
+      // Find next element sibing
+      $sibling = $blockquote_element;
+      $next_element_sibling = null;
+      while ($sibling = $sibling->nextSibling) {
+        if ($sibling->nodeType == 1) {
+          $next_element_sibling = $sibling;
+          break;
+        }
+      }
+      if ($next_element_sibling) {
+        // Find cite
+        $cite_element = $doc->getElementsByTagName( 'cite' )->item(0);
+        if ($cite_element) {
+          // Create footer
+          $blockquote_footer_element = $doc->createElement($blockquote_footer_tag);
+          // Add blockquote-footer class
+          $blockquote_footer_element->setAttribute('class', $blockquote_footer_class);
+          // Copy children
+          foreach ($next_element_sibling->childNodes as $child) {
+            $blockquote_footer_element->appendChild($child->cloneNode(true));
+          }
+        }
+        // Insert before original element
+        $blockquote_element->appendChild($blockquote_footer_element);
+        // Remove original element
+        $next_element_sibling->parentNode->removeChild($next_element_sibling);
+      }
+    }
+    
+    // Tables
+    $table_nodes = $doc->getElementsByTagName( 'table' );
+    foreach ($table_nodes as $table_node) {
+      $class = $table_node->getAttribute('class');
+      $class.= ' table';
+      $table_node->setAttribute('class', $class);
+    }
+    
+    return preg_replace('~(?:<\?[^>]*>|<(?:!DOCTYPE|/?(?:html|head|body))[^>]*>)\s*~i', '', $doc->saveHTML());
   }
-  add_filter( 'the_content', 'wp_bootstrap_the_content', 11 );  
+  add_filter( 'the_content', 'wp_bootstrap_the_content', 11 );
+  
+  
+  /**
+   * Img Caption
+   * Reference: http://wordpress.stackexchange.com/questions/36772/image-captions-have-a-10px-extra-margin-and-its-not-css
+   */
+  function wp_bootstrap_img_caption( $empty_string, $attributes, $content ) {
+
+    // Extract options
+    extract(wp_bootstrap_get_content_options());
+    
+    // Extract shortcode attributes
+    extract(shortcode_atts(array(
+      'id' => '',
+      'align' => 'alignnone',
+      'width' => '',
+      'caption' => ''
+    ), $attributes));
+    
+    // Skip if caption text is empty
+    if ( empty($caption) ) {
+      return $content;
+    }
+    
+    $id_attr = isset($id) ? '"id="' . esc_attr($id) . '"' : "";
+    
+    $style_string = "";
+    if ($width) {
+      $styles[] = "width: " . $width . "px";
+    }
+    $style_string = implode("; ", $styles);
+    $style_attr = strlen($style_string) ? "style=\"$style_string\"" : "";
+    
+    $attr_string = " " . implode(" ", array($id_attr, $style_attr));
+
+    // Add default classes
+    $caption_element_class = "wp-caption " . esc_attr($align);
+    
+    // Add caption class
+    $caption_element_class.= " $img_caption_class";
+    
+    // Add align class
+    if (preg_match('/\balignleft\b/i', $caption_element_class)) {
+      $caption_element_class.= " $align_left_class";
+    } else if (preg_match('/\balignright\b/i', $caption_element_class)) {
+      $caption_element_class.= " $align_right_class";
+    } else if (preg_match('/\baligncenter\b/i', $caption_element_class)) {
+      $caption_element_class.= " $align_center_class";
+    }
+    
+    // Get content
+    $content = do_shortcode( $content );
+    
+    return
+    
+<<<EOT
+    <$img_caption_tag$attr_string class="$caption_element_class">
+      $content
+      <$img_caption_text_tag class="wp-caption-text $img_caption_text_class">$caption</$img_caption_text_tag>
+    </$img_caption_tag>
+EOT;
+  }
+  
+  add_filter( 'img_caption_shortcode', 'wp_bootstrap_img_caption', 10, 3 );
+  
+  
+  /**
+   * Add responsive image class to thumbnail
+   */
+  function wp_bootstrap_post_thumbnail_html($html, $post_id, $post_thumbnail_id, $size, $attr) {
+    // Extract options
+    extract(wp_bootstrap_get_content_options());
+    // Parse DOM
+    $doc = new DOMDocument();
+    @$doc->loadHTML('<?xml encoding="utf-8" ?>' . $html );
+    // Handle image
+    $image_elements = $doc->getElementsByTagName( 'img' );
+    foreach ($image_elements as $image_element) {
+      // Add configured image class
+      $image_element_class = $image_element->getAttribute('class');
+      $image_element_class = trim($image_element_class . " " . $image_class);
+      $image_element->setAttribute('class', $image_element_class);
+    }
+    return preg_replace('~(?:<\?[^>]*>|<(?:!DOCTYPE|/?(?:html|head|body))[^>]*>)\s*~i', '', $doc->saveHTML());
+  }
+  
+  add_filter( 'post_thumbnail_html', 'wp_bootstrap_post_thumbnail_html', 100, 5 );
+  
+  /**
+   * Additional Styles
+   */
+  function wp_bootstrap_content_styles() {
+    echo <<<EOT
+    <style type="text/css">
+      [class*='wp-image'] {
+        max-width: 100%;
+        height: auto;
+      }
+      .alignleft {
+        margin-right: 1rem;
+      }
+      .alignright {
+        margin-left: 1rem;
+      }
+      .figure {
+        max-width: 100%;
+      }
+      .figure.aligncenter {
+        display: block;
+      }
+    </style>
+EOT;
+  }
+  add_action('wp_head', 'wp_bootstrap_content_styles', 100);
 }
 ?>
