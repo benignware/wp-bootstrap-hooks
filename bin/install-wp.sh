9656de32924e044999d8644670d8bb52d4297d12
Rafael Nowrotek <mailbenignware.com>
2018-10-18 21:55:10 +0200

    Add wp-cli install script

---
+++ bin/install-wp.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# command: sh -c "sleep 20 && wp core install --path=/var/www/html --url=localhost --title=test --admin_user=test --admin_password=test --admin_email=test@example.com"
+
+# Install WordPress.
+wp core install \
+  --title="Project" \
+  --admin_user="wordpress" \
+  --admin_password="wordpress" \
+  --admin_email="admin@example.com" \
+  --url="http://localhost:9030" \
+  --skip-email
+
+# Update permalink structure.
+wp option update permalink_structure "/%year%/%monthnum%/%postname%/" --skip-themes --skip-plugins
+
+# Activate plugin.
+#wp plugin activate my-plugin
