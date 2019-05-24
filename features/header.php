a26ae630d16c3499fff844fe798c2504afcc9b38
Rafael Nowrotek <mailbenignware.com>
2019-05-24 14:12:06 +0200

    Add img class to header image

--- features/header.php
+++ features/header.php
@@ -1,7 +1,8 @@
 <?php
 
 add_filter('get_header_image_tag', function($html, $header, $attr) {
-  $html = wp_bootstrap_tag_add_class('img', 'img-fluid', $html);
+  $options = wp_bootstrap_options();
+  $html = wp_bootstrap_tag_add_class('img', $options['img_class'], $html);
 
   return $html;
 }, 11, 3);
