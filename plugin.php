efa77c41cac3c93d5bc3b9cca1705d6abf85963d
Rafael Nowrotek <mailbenignware.com>
2019-06-16 22:30:49 +0200

    Fix missing extension in require

--- plugin.php
+++ plugin.php
@@ -4,10 +4,10 @@
  Plugin Name: Bootstrap Hooks
  Plugin URI: http://github.com/benignware/wp-bootstrap-hooks
  Description: A collection of action and filters for bootstrap based themes
- Version: 0.1.0-alpha.2
+ Version: 0.1.0-alpha.3
  Author: Rafael Nowrotek, Benignware
  Author URI: http://benignware.com
  License: MIT
 */
 
-require_once './bootstrap-hooks';
+require_once 'bootstrap-hooks.php';
