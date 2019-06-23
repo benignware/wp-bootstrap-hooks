03890b73f89c30bc4a5704c118c386c1dbf30111
Rafael Nowrotek <mailbenignware.com>
2019-06-23 14:14:35 +0200

    Bump up version

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  Plugin Name: Bootstrap Hooks
  Plugin URI: http://github.com/benignware/wp-bootstrap-hooks
  Description: A collection of action and filters for bootstrap based themes
- Version: 0.1.0-alpha.9
+ Version: 0.1.0-alpha.11
  Author: Rafael Nowrotek, Benignware
  Author URI: http://benignware.com
  License: MIT
