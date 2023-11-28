3b33f7d48662c2933cf58f78de2051973ea6b475
Rafael Nowrotek <rafaelnowrotek@MacBook-Pro-von-Rafael-2.local>
2023-11-28 23:47:17 +0100

    Fix filter not returning input value, hence undefined variable

--- features/header.php
+++ features/header.php
@@ -2,7 +2,7 @@
 
 add_filter('get_header_image_tag_attributes', function($attr) {
   if (!current_theme_supports('bootstrap')) {
-    return $html;
+    return $attr;
   }
 
   $options = wp_bootstrap_options();
