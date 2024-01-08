47d609e4ce2ac966c73b5ac23c197946bb5b20e6
Rafael Nowrotek <rafaelnowrotek@MBPvonRafael2.fritz.box>
2024-01-08 17:49:32 +0100

    Add gallery

--- features/gallery.php
+++ features/gallery.php
@@ -1,150 +1,241 @@
 <?php
-/**
- * Post Gallery
- */
-add_filter('post_gallery', function($output, $atts) {
-  if (!current_theme_supports('bootstrap')) {
-    return $output;
+
+function _bootstrap_gallery_render($template, $data = array()) {
+  if (!file_exists($template)) {
+    return '';
   }
 
-  global $post;
-  extract(wp_bootstrap_options());
-
-  if (isset($atts['orderby'])) {
-      $atts['orderby'] = sanitize_sql_orderby($atts['orderby']);
-      if (!$atts['orderby'])
-          unset($atts['orderby']);
+  foreach($data as $key => $value) {
+    $$key = $data[$key];
   }
 
-  $gallery_thumbnail_size = isset($atts['size']) ? $atts['size'] : $gallery_thumbnail_size;
-
-  $atts = shortcode_atts(array(
-      'order' => 'ASC',
-      'orderby' => 'menu_order ID',
-      'id' => $post->ID,
-      //'itemtag' => 'dl',
-      //'icontag' => 'dt',
-      //'captiontag' => 'dd',
-      'columns' => 3,
-      'size' => 'thumbnail',
-      'include' => '',
-      'exclude' => ''
-  ), $atts);
-
-  $id = intval($atts['id']);-
-  $columns = intval($atts['columns']);
-
-  if ( ! empty( $atts['include'] ) ) {
-    $_attachments = get_posts( array( 'include' => $atts['include'], 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $atts['order'], 'orderby' => $atts['orderby'] ) );
-    $attachments = array();
-    foreach ( $_attachments as $key => $val ) {
-      $attachments[$val->ID] = $_attachments[$key];
-    }
-  } elseif ( ! empty( $atts['exclude'] ) ) {
-    $attachments = get_children( array( 'post_parent' => $id, 'exclude' => $atts['exclude'], 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $atts['order'], 'orderby' => $atts['orderby'] ) );
-  } else {
-    $attachments = get_children( array( 'post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $atts['order'], 'orderby' => $atts['orderby'] ) );
-  }
-
-  if (empty($attachments)) return '';
-
-  // get image size
-  $image_size = null;
-  global $_wp_additional_image_sizes;
-
-
-  $gallery_id = uniqid();
-
-  $col_value = floor(12 / $columns);
-
-
-  // TODO: Template
-  // Start generating output
-  $output = "<div class=\"gallery\" id=\"gallery-$gallery_id\">\n";
-  $output.= "  <div class=\"row\">\n";
-
-  // Loop through each attachment and create thumbnails
-  foreach ($attachments as $id => $attachment) {
-    $img_thumb = wp_get_attachment_image_src($id, $gallery_thumbnail_size);
-    $output.= "   <div class=\"col-md-$col_value col-sm-$col_value col-xs-$col_value\">";
-    $output.= "     <a class=\"$gallery_thumbnail_class\" data-id=\"$id\">\n";
-    // Fetch the thumbnail (or full image, it's up to you)
-    $output.= "       <img class=\"$gallery_thumbnail_img_class\" src=\"{$img_thumb[0]}\" title=\"{$attachment->post_excerpt}\" alt=\"{$attachment->post_excerpt}\" />\n";
-    $output.= "     </a>\n";
-    $output.= "   </div>\n";
-  }
-  $output .= "  </div>\n";
-  $output .= "</div>\n";
-
-  $output.= "<div class=\"modal modal-carousel modal-gallery fade modal-fullscreen force-fullscreen\" id=\"modal-gallery-$gallery_id\" role=\"dialog\">";
-  $output.= '  <div class="modal-dialog modal-lg">';
-  $output.= '    <div class="modal-content">';
-  $output.= '      <div class="modal-header">';
-  $output.= '        <button class="close" type="button" data-dismiss="modal">×</button>';
-  $output.= '        <h3 class="modal-title">' . $post->post_title . '</h3>';
-  $output.= '      </div>';
-  $output.= '      <div class="modal-body">';
-  $output.= "        <div id=\"carousel-gallery-$gallery_id\" class=\"carousel slide carousel-fit\" data-ride=\"carousel\">";
-  $output.= '          <ol class="carousel-indicators">';
-  $image_index = 0;
-  foreach ($attachments as $id => $attachment) {
-    $output.= "          <li data-target=\"#carousel-gallery-$gallery_id\" data-slide-to=\"$image_index\" data-id=\"$id\"></li>";
-    $image_index++;
-  };
-  $output.= '          </ol>';
-  $output.= '          <div class="carousel-inner">';
-  $image_index = 0;
-
-  foreach ($attachments as $id => $attachment) {
-
-    $img_large = wp_get_attachment_image_src($id, $gallery_zoom_size);
-
-    $active = $image_index === 0 ? 'active' : '';
-    $output.= "          <div class=\"$carousel_item_class $active\" data-id=\"$id\">";
-    $output.= "            <img src=\"{$img_large[0]}\" title=\"{$attachment->post_excerpt}\" alt=\"{$attachment->post_excerpt}\" />\n";
-    $output.= '            <div class="carousel-caption">';
-    $output.= "              <p>{$attachment->post_excerpt}</p>";
-    $output.= '            </div>';
-    $output.= '          </div>';
-    $image_index++;
-  }
-  $output.= '          </div>';
-  $output.= "          <a class=\"carousel-control left\" href=\"#carousel-gallery-$gallery_id\" data-slide=\"prev\"><i class=\"icon-left icon-prev\"></i></a>";
-  $output.= "          <a class=\"carousel-control right\" href=\"#carousel-gallery-$gallery_id\" data-slide=\"next\"><i class=\"icon-right icon-next\"></i></a>";
-  $output.= '        </div>';
-  $output.= '      </div>';
-  $output.= '      <div class="modal-footer">';
-  $output.= '        <button class="' . $close_button_class . '" data-dismiss="modal">' . $close_button_label . '</button>';
-  $output.= '      </div>';
-  $output.= '    </div>';
-  $output.= '  </div>';
-  $output.= '</div>';
-  // Script
-  $output.= "<script>\n";
-  $output.= "(function($) {\n";
-  $output.= "  var\n";
-  $output.= "    galleryId = '$gallery_id',\n";
-  $output.= "    \$gallery = $('#gallery-' + galleryId),\n";
-  $output.= "    \$modal = $('#modal-gallery-' + galleryId),\n";
-  $output.= "    \$carousel = $('#carousel-gallery-' + galleryId);\n";
-  $output.= "    \$gallery.find('a[data-id]').on('ontouchend' in window ? 'touchend' : 'click', function(event) {\n";
-  $output.= "      \$carousel.find('.carousel-item').removeClass('active');\n";
-  $output.= "      \$carousel.find('.carousel-item').removeClass('next');\n";
-  $output.= "      \$carousel.find('.carousel-item').removeClass('left');\n";
-  $output.= "      \$carousel.find('.carousel-indicators li').removeClass('active');\n";
-  $output.= "      var\n";
-  $output.= "        itemId = $(this).data('id');\n";
-  $output.= "        \$carousel.find(\".carousel-item[data-id='\" + itemId + \"']\").addClass('active');\n";
-  $output.= "        \$carousel.find(\".carousel-indicators li[data-id='\" + itemId + \"']\").addClass('active');\n";
-  //$output.= "      \$modal.find('.modal-title').html(\$carousel.find('.active img').attr('title'));";
-  $output.= "      \$modal.modal('show');\n";
-  $output.= "    });\n";
-  //$output.= "    \$carousel.on('slid.bs.carousel', function () {\n";
- // $output.= "    \$modal.find('.modal-title').html(\$carousel.find('.active img').attr('title'));";
-  //$output.= "    });\n";
-  $output.= "    console.log('xxx script: ', \$gallery, \$modal, \$carousel);";
-  $output.= "})(jQuery)";
-  $output.= "</script>";
+  ob_start();
+  include $template;
+  $output = ob_get_contents();
+  ob_end_clean();
 
   return $output;
-}, 10, 2 );
+}
+
+function bootstrap_gallery($params, $content = null) {
+	global $wp, $wp_query, $post;
+
+	$params = array_merge([
+		'template' => 'carousel',
+		'format' => '',
+    'id' => null,
+    'title' => '',
+    'class' => 'gallery',
+    'columns' => 3,
+		// Query params
+		'ids' => null,
+		'order' => '',
+    'orderby' => '',
+    'post_status' => 'publish',
+    'nopaging' => true,
+    'post_type' => 'any',
+    'post_mime_type' => null,
+    'include' => '',
+    'exclude' => '',
+    // Gallery
+    'post_type' => 'attachment',
+    'post_status' => 'inherit',
+    'post_mime_type' => 'image',
+    'ids' => is_array($params['ids']) ? implode(',', $params['ids']) : $params['ids'],
+    'size' => 'large',
+    'fit' => 'cover',
+    'autoplay' => false,
+    'interval' => 3000,
+    'captions' => null,
+    'align' => '',
+    'attrs' => [],
+    'fullscreen' => true,
+    'thumbnails' => true
+  ], $params);
+
+  $post_title = !$params['title'] ? get_the_title() : ''; // Alternatively show post title on gallery
+
+  $template_locations = [
+    get_template_directory() . '/bootstrap/gallery',
+    get_stylesheet_directory() . '/bootstrap/gallery',
+    __DIR__ . '/../template/gallery'
+  ];
+
+  $template = $params['template'];
+
+  $template_name = $template ? sprintf('gallery-%s.php', $template) : 'gallery.php';
+  $template_file = array_reduce($template_locations, function($result, $dir) use ($template_name) {
+    $file = $dir . '/' . $template_name;
+
+    return $result || file_exists($file) ? $file : null;
+  }, null);
+
+  $params['autoplay'] = $params['autoplay'] === 'false' ? false : $params['autoplay'];
+
+  // Parse booleans
+  $params = array_map(function($value) {
+		return $value === 'false' ? false : ($value === 'true' ? true : $value);
+	}, $params);
+
+  if (empty($params['id'])) {
+    $params['id'] = sanitize_title(
+      sprintf(
+        'sitekick-gallery-%s-%s',
+        $post ? $post->ID : '0',
+        base64_encode(str_replace(',', '-', $params['ids']))
+      )
+    );
+  }
+
+  // Transform ids to query params
+	if ( ! empty( $params['ids'] ) ) {
+    // 'ids' is explicitly ordered, unless you specify otherwise.
+    if ( empty( $params['orderby'] ) ) {
+      $params['orderby'] = 'post__in';
+    }
+
+    $params['include'] = $params['ids'];
+  }
+
+  $query_params = array_merge(
+    array_intersect_key($params, array_flip([
+      'order',
+      'orderby',
+      'include',
+      'exclude',
+      'post_type',
+      'post_mime_type',
+      'post_status',
+      'nopaging'
+    ])),
+    array(
+      'post__in' => is_array($params['include']) ? $params['include'] : explode(',', $params['include']),
+      'orderby' => $params['order'] === 'RAND' ? 'none' : $params['orderby']
+    )
+  );
+
+	$wp_query = new WP_QUERY($query_params);
+
+  if (!count($wp_query->posts)) {
+    return '';
+  }
+
+  $captions = isset($params['captions'])
+    ? $params['captions']
+    : (
+      is_array($params['include']) ? array_fill('', count( $params['include'])) : []
+    );
+  $captions = is_string($captions) ? explode(',', $captions) : $captions;
+
+  foreach ($wp_query->posts as $index => $post) {
+    if (!isset($captions[$index]) || !$captions[$index]) {
+      $captions[$index] = wp_get_attachment_caption($post->ID);
+    }
+  }
+
+  $class = implode(' ', [
+    $params['class'],
+    // $attrs['class'],
+    // "align$align"
+  ]);
+
+  $attrs['class'] = $class;
+
+  $data = array_merge([
+    'id' => $params['id'],
+    'columns' => $params['columns'],
+    'title' => $params['title'],
+    'post_title' => $post_title,
+    'size' => $params['size'],
+    'autoplay' => $params['autoplay'],
+    'interval' => $params['interval'],
+    'captions' => $captions,
+    'wp_query' => $wp_query,
+    'fit' => $params['fit'],
+    'align' => $params['align'],
+    'attrs' => $params['attrs'],
+    'fullscreen' => $params['fullscreen'],
+    'thumbnails' => $params['thumbnails']
+  ]);
+
+	$output = _bootstrap_gallery_render($template_file, $data);
+
+	wp_reset_query();
+
+	return $output;
+}
+
+add_filter('render_block', function($html, $block = null) {
+  if ($block['blockName'] === 'core/gallery') {
+    $attrs = $block['attrs'];
+    $captions = [];
+
+    foreach ($block['innerBlocks'] as $index => $inner_block) {
+      if (isset($inner_block['innerHTML'])) {
+        $inner_html = $inner_block['innerHTML'];
+        $doc = new DOMDocument();
+        @$doc->loadHTML('<?xml encoding="utf-8" ?>' . $inner_html);
+        $doc_xpath = new DOMXpath($doc);
+        $fig_caption = $doc_xpath->query("//figcaption")->item(0);
+        $captions[$index] = $fig_caption ? $fig_caption->textContent : '';
+      } else {
+        $captions[$index] = '';
+      }
+    }
+  
+    if (isset($attrs['ids'])) {
+      $ids = is_array($attrs['ids']) ? $attrs['ids'] : implode(',', $attrs['ids']);
+    } else if (isset($block['innerBlocks'])) {
+      $ids = [];
+      $inner_blocks = $block['innerBlocks'];
+
+      foreach ($inner_blocks as $index => $item) {
+        if (isset($item['attrs']) and isset($item['attrs']['id'])) {
+          $id = $item['attrs']['id'];
+          array_push($ids, $id);
+        }
+      }
+    }
+
+    $ids = is_array($ids) ? implode(',', $ids) : $ids;
+
+    $doc = new DOMDocument();
+    @$doc->loadHTML('<?xml encoding="utf-8" ?>' . $html);
+    $doc_xpath = new DOMXpath($doc);
+
+    $gallery_caption_class = 'blocks-gallery-caption';
+    $gallery_caption = $doc_xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $gallery_caption_class ')]")->item(0);
+
+    $title = '';
+
+    if ($gallery_caption) {
+      $title = $gallery_caption->textContent;
+    }
+
+    $columns = isset($attrs['columns']) && !empty($attrs['columns']) ? $attrs['columns'] : 3;
+    $size = isset($attrs['sizeSlug']) ? $attrs['sizeSlug'] : 'large';
+    $fit = !isset($attrs['imageCrop']) || $attrs['imageCrop'] ? 'cover' : false;
+
+    $align = isset($attrs['align']) ? $attrs['align'] : '';
+
+    $html = bootstrap_gallery([
+      'ids' => $ids,
+      'columns' => $columns,
+      'title' => $title,
+      'size' => $size,
+      'captions' => $captions,
+      'fit' => $fit,
+      'align' => $align
+    ]);
+  }
+
+  return $html;
+}, 10, 2);
+
+
+add_filter( 'post_gallery', function($output, $attr, $instance) {
+  $html = bootstrap_gallery($attr);
+
+  return $html;
+}, 10, 3);
+
---
+++ template/gallery/gallery-thumbnails.php
@@ -0,0 +1,75 @@
+<div
+  <?php $column_width = 72 ?>
+  <?php foreach (array_merge($attrs, [
+    'id' => $id . '-thumbnails',
+    'class' => 'mt-2 d-grid column-gap-2 overflow-hidden',
+    'style' => "grid-template-columns: repeat(auto-fill, minmax({$column_width}px, 1fr)); grid-template-rows: 1fr; grid-auto-rows: 0;"
+  ]) as $name => $value): ?>
+    <?= $name ?>="<?= $value ?>"
+  <?php endforeach; ?>
+>
+<?php while( have_posts()) : the_post() ?>
+  <?= wp_get_attachment_image(get_the_ID(), 'small', false, [
+    'class' => "figure-img img-fluid m-0 h-100 object-fit-cover" . ($wp_query->current_post === 0 ? ' active' : ''),
+    'style' => "aspect-ratio: 1/1; border-radius: var(--bs-border-radius); cursor: pointer; height: 100%; max-height: 175px; width: auto",
+    'data-bs-target' => "#$id-carousel",
+    'data-bs-slide-to' => $wp_query->current_post
+  ]) ?>
+<?php endwhile; ?>
+</div>
+
+<style>
+  #<?= $id ?>-thumbnails > * {
+    opacity: 0.6;
+    transition: opacity 0.25s ease;
+  }
+
+  #<?= $id ?>-thumbnails > .active {
+    opacity: 1;
+  }
+</style>
+<script>
+  (() => {
+    const COLUMN_WIDTH = <?= $column_width ?>;
+    const thumbnails = document.getElementById('<?= $id ?>-thumbnails');
+
+    const resize = () => {
+      const style = window.getComputedStyle(thumbnails);
+      const g = parseFloat(style.getPropertyValue('column-gap'));
+      const w = thumbnails.clientWidth + g;
+      
+      const a = Math.floor(w / (COLUMN_WIDTH - g));
+      const c = thumbnails.childElementCount;
+      const x = w / a - g;
+
+      thumbnails.style.gridTemplateColumns = `repeat(${Math.max(c, a)}, minmax(${x}px, 1fr))`;
+    };
+
+    window.addEventListener('resize', resize);
+
+    resize();
+
+    const carousel = document.getElementById('<?= $id ?>-carousel');
+
+    carousel.addEventListener('slide.bs.carousel', event => {
+      const items = [...thumbnails.children];
+      const slideIndex = event.to;
+      const itemIndex = Math.max(0, event.to - 2);
+      const item = items[itemIndex];
+
+      const bi = item.getBoundingClientRect();
+      const bp = thumbnails.getBoundingClientRect();
+      const x = thumbnails.scrollLeft + bi.left - bp.left;
+
+      items.forEach((item, index) => {
+        item.classList.toggle('active', slideIndex === index);
+      });
+
+      thumbnails.scrollTo({
+        left: x,
+        behavior: 'smooth'
+      });
+    });
+    
+  })();
+</script>
