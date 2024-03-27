8c28fde6437af57a2d546c9cc369ae7e53d13379
Rafael Nowrotek <mail@benignware.com>
2024-03-27 18:48:47 +0100

    Update cdn Bootstrap

--- test/themes/bootstrap-blocks/functions.php
+++ test/themes/bootstrap-blocks/functions.php
@@ -15,20 +15,20 @@ add_action( 'after_setup_theme', function() {
 
 	add_theme_support( 'editor-styles' );   
 	
-	add_editor_style( 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' );
+	add_editor_style( 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' );
 } ); 
 
 add_action( 'wp_enqueue_scripts', function() {
 	wp_enqueue_style(
 		'bootstrap',
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+    'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
 		array(),
 		wp_get_theme()->get( 'Version' )
 	);
 
 	wp_enqueue_script(
 		'bootstrap',
-		'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',
+		'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',
 		array(),
 		wp_get_theme()->get( 'Version' )
 	);
